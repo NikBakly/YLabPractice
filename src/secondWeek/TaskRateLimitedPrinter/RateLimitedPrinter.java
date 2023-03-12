425fd8802ef8ee4c6cda2d52acf43224b0079c96
baklyNik <baklykov.nikita369@gmail.com>
2023-03-13 02:12:01 +0300

    solve RateLimitedPrinter task

--- src/secondWeek/TaskRateLimitedPrinter/RateLimitedPrinter.java
+++ src/secondWeek/TaskRateLimitedPrinter/RateLimitedPrinter.java
@@ -1,11 +1,31 @@
 package secondWeek.TaskRateLimitedPrinter;
 
 public class RateLimitedPrinter {
+    private final int interval;
+    private long lastTimeUpdate;
+
     public RateLimitedPrinter(int interval) {
-// code here
+        checkInterval(interval);
+        this.interval = interval;
+
     }
 
     public void print(String message) {
-// code here
+        if (lastTimeUpdate == 0) {
+            System.out.println(message);
+            lastTimeUpdate = System.currentTimeMillis();
+        } else {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - lastTimeUpdate > interval) {
+                System.out.println(message);
+                lastTimeUpdate = System.currentTimeMillis();
+            }
+        }
+    }
+
+    private void checkInterval(int interval) {
+        if (interval < 0) {
+            throw new RuntimeException("Интервал не может быть отрицательным числом");
+        }
     }
 }
--- src/secondWeek/TaskRateLimitedPrinter/RateLimitedPrinterTest.java
+++ src/secondWeek/TaskRateLimitedPrinter/RateLimitedPrinterTest.java
@@ -2,6 +2,10 @@ package secondWeek.TaskRateLimitedPrinter;
 
 public class RateLimitedPrinterTest {
     public static void main(String[] args) {
+        RateLimitedPrinter rateLimitedPrinter = new RateLimitedPrinter(-1);
+        for (int i = 0; i < 1_000_000_000; i++) {
+            rateLimitedPrinter.print(String.valueOf(i));
+        }
 
     }
 }
