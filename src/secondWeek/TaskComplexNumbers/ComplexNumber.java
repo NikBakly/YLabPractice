b8efacb024df9a1e7b92e016720dbbd46ea60e8e
baklyNik <baklykov.nikita369@gmail.com>
2023-03-13 01:35:46 +0300

    solve Complex Numbers task

--- src/secondWeek/TaskComplexNumbers/ComplexNumber.java
+++ src/secondWeek/TaskComplexNumbers/ComplexNumber.java
@@ -1,4 +1,61 @@
 package secondWeek.TaskComplexNumbers;
 
 public class ComplexNumber {
+    // действительная часть
+    private final double re;
+
+    // мнимая часть
+    private final double im;
+
+    public ComplexNumber(double re) {
+        this.re = re;
+        this.im = 0;
+    }
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public ComplexNumber plus(ComplexNumber secondNumber) {
+        return new ComplexNumber(
+                this.re + secondNumber.re,
+                this.im + secondNumber.im
+        );
+    }
+
+    public ComplexNumber minus(ComplexNumber secondNumber) {
+        return new ComplexNumber(
+                this.re - secondNumber.re,
+                this.im - secondNumber.im
+        );
+    }
+
+    public ComplexNumber multiplication(ComplexNumber secondNumber) {
+        // Пояснение. Пусть имеются два комплексных числа: z1 = a1 + i * b1, z2 = a2 + i * b2;
+        // Тогда z1 * z2 = (a1 + i * b1) * (a2 + i * b2) =>
+        // => [раскрываем скобки и сгруппируем по действительной и мнимой части] => (a1 * a2 - b1 * b2) + i * (a1 * b2 + b1 * a2);
+        // Таким образом у нас получилась общая формула нахождения произведений для комплексных чисел.
+        return new ComplexNumber(
+                (this.re * secondNumber.re - this.im * secondNumber.im),
+                (this.re * secondNumber.im + this.im * secondNumber.re)
+        );
+    }
+
+    public Double getModule() {
+        return Math.sqrt(this.re * this.re + this.im * this.im);
+    }
+
+    @Override
+    public String toString() {
+        String result;
+        if (im < 0) {
+            result = String.format("z = %s - i * %s", re, -im);
+        } else if (im > 0) {
+            result = String.format("z = %s + i * %s", re, im);
+        } else {
+            result = String.format("z = %s", re);
+        }
+        return result;
+    }
 }
--- src/secondWeek/TaskComplexNumbers/ComplexNumberTest.java
+++ src/secondWeek/TaskComplexNumbers/ComplexNumberTest.java
@@ -1,4 +1,20 @@
 package secondWeek.TaskComplexNumbers;
 
 public class ComplexNumberTest {
+    public static void main(String[] args) {
+        ComplexNumber z1 = new ComplexNumber(5, 4);
+        ComplexNumber z2 = new ComplexNumber(2, 3);
+        System.out.println(z1.plus(z2)); // результат: z = 7 + i * 7
+        System.out.println(z1.minus(z2)); // результат: z = 3 + i * 1
+        System.out.println(z2.minus(z1)); // результат: z = -3 - i * 1
+        System.out.println(z1.multiplication(z2)); // результат: z = -2 + i * 23
+        System.out.println(z1.getModule()); // результат: 6.403
+        System.out.println(z2.getModule()); // результат: 3.606
+
+
+        ComplexNumber z3 = new ComplexNumber(5);
+        System.out.println(z3); // результат: z = 5
+        System.out.println(z3.plus(z1)); // результат: z = 10 + i * 4
+        System.out.println(z3.getModule()); // результат: 5
+    }
 }
