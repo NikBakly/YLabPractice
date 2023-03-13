4232a56c5a58c10e1ef525384da5df0214d52942
baklyNik <baklykov.nikita369@gmail.com>
2023-03-13 17:07:32 +0300

    solve Snils Validator task

--- src/secondWeek/TaskSnilsValidator/SnilsValidatorTest.java
+++ src/secondWeek/TaskSnilsValidator/SnilsValidatorTest.java
@@ -2,6 +2,10 @@ package secondWeek.TaskSnilsValidator;
 
 public class SnilsValidatorTest {
     public static void main(String[] args) {
-
+        SnilsValidator snilsValidator = new SnilsValidatorImpl();
+        System.out.println(snilsValidator.validate("48095351208")); // true
+        System.out.println(snilsValidator.validate("480-953-512-08")); // true
+        System.out.println(snilsValidator.validate("480-953-512 08")); // true
+        System.out.println(snilsValidator.validate("48895333208")); // false
     }
 }
