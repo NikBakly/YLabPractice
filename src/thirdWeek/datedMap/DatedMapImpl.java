f6f097eb9ebba6e015585df263deba34d5cc92fe
baklyNik <baklykov.nikita369@gmail.com>
2023-03-19 11:40:14 +0300

    solve DatedMap task

---
+++ src/thirdWeek/datedMap/DatedMapImpl.java
@@ -0,0 +1,53 @@
+package thirdWeek.datedMap;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class DatedMapImpl implements DatedMap {
+    private final Map<String, ValueWithDate> map = new HashMap<>();
+
+    @Override
+    public void put(String key, String value) {
+        ValueWithDate newValue = new ValueWithDate(
+                value,
+                new Date(Instant.now().toEpochMilli())
+        );
+        map.put(key, newValue);
+    }
+
+    @Override
+    public String get(String key) {
+        String result = null;
+        if (containsKey(key)) {
+            result = map.get(key).getValue();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        return map.containsKey(key);
+    }
+
+    @Override
+    public void remove(String key) {
+        map.remove(key);
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return map.keySet();
+    }
+
+    @Override
+    public Date getKeyLastInsertionDate(String key) {
+        Date result = null;
+        if (containsKey(key)) {
+            result = map.get(key).getDate();
+        }
+        return result;
+    }
+}
