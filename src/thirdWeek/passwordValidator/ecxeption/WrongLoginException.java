4c3eee9e6a1947de31fe47481d91a76fa79c5a38
baklyNik <baklykov.nikita369@gmail.com>
2023-03-19 17:33:01 +0300

    solve PasswordValidator task

---
+++ src/thirdWeek/passwordValidator/ecxeption/WrongLoginException.java
@@ -0,0 +1,10 @@
+package thirdWeek.passwordValidator.ecxeption;
+
+public class WrongLoginException extends Exception {
+    public WrongLoginException() {
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
---
+++ src/thirdWeek/passwordValidator/ecxeption/WrongPasswordException.java
@@ -0,0 +1,10 @@
+package thirdWeek.passwordValidator.ecxeption;
+
+public class WrongPasswordException extends Exception {
+    public WrongPasswordException() {
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
