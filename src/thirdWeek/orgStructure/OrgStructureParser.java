e88bcba570f0302e91a75351f26854c9cef24c39
baklyNik <baklykov.nikita369@gmail.com>
2023-03-19 15:37:48 +0300

    solve Org Structure task

---
+++ src/thirdWeek/orgStructure/OrgStructureParser.java
@@ -0,0 +1,8 @@
+package thirdWeek.orgStructure;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface OrgStructureParser {
+    Employee parseStructure(File csvFile) throws IOException;
+}
