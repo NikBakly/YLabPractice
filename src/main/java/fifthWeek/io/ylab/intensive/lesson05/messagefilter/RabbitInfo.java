d685f71e7014447f94efc0a029f9ef148254aaac
baklyNik <baklykov.nikita369@gmail.com>
2023-04-02 13:45:01 +0300

    solve Message Filter task

---
+++ src/main/java/fifthWeek/io/ylab/intensive/lesson05/messagefilter/RabbitInfo.java
@@ -0,0 +1,11 @@
+package fifthWeek.io.ylab.intensive.lesson05.messagefilter;
+
+public class RabbitInfo {
+    public static String QUEUE_NAME_INPUT = "input";
+    public static String QUEUE_NAME_OUTPUT = "output";
+    public static String EXCHANGE_NAME = "exc";
+
+    private RabbitInfo() {
+
+    }
+}
