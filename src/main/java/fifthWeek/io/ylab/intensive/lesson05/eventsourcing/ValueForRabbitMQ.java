e1e298098615db68a3e0f01379405520e33ebf84
baklyNik <baklykov.nikita369@gmail.com>
2023-04-11 20:58:17 +0300

    some refactoring

--- src/main/java/fifthWeek/io/ylab/intensive/lesson05/eventsourcing/ValueForRabbitMQ.java
+++ src/main/java/fifthWeek/io/ylab/intensive/lesson05/eventsourcing/ValueForRabbitMQ.java
@@ -1,8 +1,8 @@
 package fifthWeek.io.ylab.intensive.lesson05.eventsourcing;
 
 public class ValueForRabbitMQ {
-    public static String QUEUE_NAME = "edit";
-    public static String EXCHANGE_NAME = "exc";
+    public final static String QUEUE_NAME = "edit";
+    public final static String EXCHANGE_NAME = "exc";
 
     private ValueForRabbitMQ() {
     }
